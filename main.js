9da24ff981792b953fb915030a4584fb64332ab4
Flávia lemes <105284601+Flavialemes@users.noreply.github.com>
2023-10-10 09:27:33 -0300

    ok

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play()
-    
+
 }
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
 
@@ -10,3 +10,5 @@ function tocaSomClap(){
 
 }
 document.querySelector('.tecla_clap').onclick = tocaSomClap;
+
+const listaDeTeclas = document.querySelectorAll('.tecla');
