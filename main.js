b5a84518f1ea8c5069982c0e8c4a4693d68df8e6
Flávia lemes <105284601+Flavialemes@users.noreply.github.com>
2023-10-17 09:07:53 -0300

    ok

--- main.js
+++ main.js
@@ -1,15 +1,10 @@
+//declaração/criando função tocaSomPom
 function tocaSomPom(){
     document.querySelector('#som_tecla_pom').play()
-
 }
+
+//chamando/invocando a função tocaSomPom pelo clique do botão
 document.querySelector('.tecla_pom').onclick = tocaSomPom;
-
-
-function tocaSomClap(){
-    document.querySelector('#som_tecla_clap').play()
-
-}
-document.querySelector('.tecla_clap').onclick = tocaSomClap;
 
 const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[1].onclick= TocaSomClap;
