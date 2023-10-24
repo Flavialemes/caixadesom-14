ef08615700c5686cbe063fd625a76ccd36b9562a
Flávia lemes <105284601+Flavialemes@users.noreply.github.com>
2023-10-24 09:32:11 -0300

    ok

--- main.js
+++ main.js
@@ -1,6 +1,6 @@
 //declaração/criando função tocaSomPom
 function tocaSom(idElementoAudio){
-    document.querySelector('#som_tecla_pom').play()
+    document.querySelector(idElementoAudio).play()
 }
 
 //chamando/invocando a função tocaSomPom pelo clique do botão
@@ -14,7 +14,11 @@ listaDeTeclas[0].onclick= tocaSom;
 
 let contador =0;
 while (contador < listaDeTeclas.length){
-listaDeTeclas[contador].onclick= tocaSomPom;
+    const efeito = listaDeTeclas[contador].classList[1];
+    const idAudio ="#som_"+ efeito;
+listaDeTeclas[contador].onclick= function(){
+
+}
 contador = contador + 1;
 console.log(contador);
 }
