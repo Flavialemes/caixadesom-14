075d84d97150d29a3a613aa18905162040ba0afc
Flávia lemes <105284601+Flavialemes@users.noreply.github.com>
2023-10-24 09:23:26 -0300

    ok

--- main.js
+++ main.js
@@ -13,7 +13,7 @@ const listaDeTeclas = document.querySelectorAll('.tecla');
 listaDeTeclas[0].onclick= tocaSomPom;
 
 let contador =0;
-while (contador < 9){
+while (contador < listaDeTeclas.length){
 listaDeTeclas[contador].onclick= tocaSomPom;
 contador = contador + 1;
 console.log(contador);
