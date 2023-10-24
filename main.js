c211eef8ab5ff2e8192da47b2e00240a521172f4
Flávia lemes <105284601+Flavialemes@users.noreply.github.com>
2023-10-24 09:27:08 -0300

    ok

--- main.js
+++ main.js
@@ -1,16 +1,16 @@
 //declaração/criando função tocaSomPom
-function tocaSomPom(){
+function tocaSom(idElementoAudio){
     document.querySelector('#som_tecla_pom').play()
 }
 
 //chamando/invocando a função tocaSomPom pelo clique do botão
-document.querySelector('.tecla_pom').onclick = tocaSomPom;
+document.querySelector('.tecla_pom').onclick = tocaSom;
 
 //referência constante listaDeTeclas, buscando todos os botões
 const listaDeTeclas = document.querySelectorAll('.tecla');
 
 //chamando a função tocaSomPom pelo item"0" da listaDeTecla
-listaDeTeclas[0].onclick= tocaSomPom;
+listaDeTeclas[0].onclick= tocaSom;
 
 let contador =0;
 while (contador < listaDeTeclas.length){
