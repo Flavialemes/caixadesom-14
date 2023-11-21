bae6413f94fefb2cb832da051e427cb189f793ae
Flávia lemes <105284601+Flavialemes@users.noreply.github.com>
2023-11-21 09:20:26 -0300

    Update main.js

--- main.js
+++ main.js
@@ -19,8 +19,12 @@ for (let contador =0;contador < listaDeTeclas.length;contador++){
     tecla.onclick= function(){
         tocaSom(idAudio)
 }
-   tecla.onkeyup = funtion()
+   tecla.onkeyup = funtion(evento){
+       if(evento.code != 'Tab')
     tecla.classList.add('ativa')
 }
-tecla.onkeyup = funtion()
+tecla.onkeyup = funtion(){
  tecla.classList.remove('ativa')
+
+}
+}
